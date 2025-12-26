680d73e4132ec914e8a076a0659c3654b40148d2
Matthew Clendenning <124844665+Matthew-Clendenning@users.noreply.github.com>
2025-12-26 15:03:27 -0600

    Fixed proxy.ts file

--- src/proxy.ts
+++ src/proxy.ts
@@ -45,3 +45,12 @@ export default clerkMiddleware(async (auth, req) => {
   await auth.protect()
   return NextResponse.next()
 })
+
+export const config = {
+  matcher: [
+    // Skip Next.js internals and all static files
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+    // Always run for API routes
+    '/(api|trpc)(.*)',
+  ],
+}
